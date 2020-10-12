2c0e93b57665e1665ac99bc7e23ab348f6d30453
Doug Schallmoser <dougschallmoser@mgail.com>
2020-10-12 15:12:26 -0700

    add method to render item html

---
+++ gastropoda-frontend/src/components/entry.js
@@ -0,0 +1,15 @@
+class Entry {
+  constructor(entry) {
+    this.id = entry.id
+    this.title = entry.title
+    this.author_name = entry.author_name
+    this.author_bio = entry.author_bio 
+    this.content = entry.content
+    this.likes = entry.likes
+    this.image = entry.image
+  }
+
+  renderItem() {
+    return `<img src="${this.image}" class="item"></img>`
+  }
+}
