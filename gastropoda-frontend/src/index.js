11a37cc20921b73dc9763891a297b53e736db0c5
Doug Schallmoser <dougschallmoser@mgail.com>
2020-10-13 18:01:04 -0700

    add function to add class when offset is reached

--- gastropoda-frontend/src/index.js
+++ gastropoda-frontend/src/index.js
@@ -1,1 +1,16 @@
 const app = new App()
+
+const navbar = document.getElementById('navbar')
+const sticky = navbar.offsetTop
+
+window.onscroll = function() {
+  navBar()
+}
+
+let navBar = function() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
