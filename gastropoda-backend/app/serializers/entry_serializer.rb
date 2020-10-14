c86e2b9c0a12faebf2b1e0af1e63fe6f95106125
Doug Schallmoser <dougschallmoser@mgail.com>
2020-10-14 10:59:59 -0700

    add created_at attribute

--- gastropoda-backend/app/serializers/entry_serializer.rb
+++ gastropoda-backend/app/serializers/entry_serializer.rb
@@ -1,4 +1,4 @@
 class EntrySerializer < ActiveModel::Serializer 
-  attributes :id, :title, :author_name, :author_bio, :content, :likes, :image
+  attributes :id, :title, :author_name, :author_bio, :content, :likes, :image, :created_at
   has_many :comments
 end
