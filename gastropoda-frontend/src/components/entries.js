08c7e582dd607db2ddbe1f0839ab9ba5d5634578
Doug Schallmoser <dougschallmoser@mgail.com>
2020-10-14 11:47:13 -0700

    add condition to loadForm method

--- gastropoda-frontend/src/components/entries.js
+++ gastropoda-frontend/src/components/entries.js
@@ -20,34 +20,35 @@ class Entries {
   }
 
   loadForm() {
-    const formDiv = document.createElement('div')
-    formDiv.className = "entry-form"
-    formDiv.innerHTML = `
-    <form id="entry-form">
-      <label for="title">Title:</label>
-      <input type="text" id="title"><p></p>
-      <label for="author_name">Author Name:</label>
-      <input type="text" id="author_name"><p></p>
-      <label for="author_bio">Author Bio:</label>
-      <input type="text" id="author_bio" name="author_bio"><p></p>
-      <label for="content">Content:</label>
-      <input type="text" id="content"><p></p>
-      <label for="image">Image URL:</label>
-      <input type="text" id="image"><p></p>
-      <input type="submit">
-    </form>`
-    this.newForm.append(formDiv)
-    this.loadFormButton.innerText = "Close"
-    this.loadFormButton.removeEventListener('click', this.loadForm.bind(this))
-    this.loadFormButton.addEventListener('click', this.closeForm.bind(this))
-    this.entryForm = document.getElementById('entry-form')
-    this.entryForm.addEventListener('submit', this.createEntry.bind(this))
+    if (document.querySelector('.entry-form')) {
+      this.closeForm()
+    } else {
+      const formDiv = document.createElement('div')
+      formDiv.className = "entry-form"
+      formDiv.innerHTML = `
+      <form id="entry-form">
+        <label for="title">Title:</label>
+        <input type="text" id="title"><p></p>
+        <label for="author_name">Author Name:</label>
+        <input type="text" id="author_name"><p></p>
+        <label for="author_bio">Author Bio:</label>
+        <input type="text" id="author_bio" name="author_bio"><p></p>
+        <label for="content">Content:</label>
+        <input type="text" id="content"><p></p>
+        <label for="image">Image URL:</label>
+        <input type="text" id="image"><p></p>
+        <input type="submit">
+      </form>`
+      this.newForm.append(formDiv)
+      this.loadFormButton.innerText = "Close"
+      this.entryForm = document.getElementById('entry-form')
+      this.entryForm.addEventListener('submit', this.createEntry.bind(this))
+    }
   }
 
   closeForm() {
     document.querySelector('.entry-form').innerHTML = ''
     this.loadFormButton.innerText = "Submit Story"
-    this.setEventListeners()
   }
 
   createEntry(event) {
