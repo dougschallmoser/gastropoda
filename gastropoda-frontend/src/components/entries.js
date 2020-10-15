a191b8c162cb41f02c1ed0000b2f4129e5d39ddc
Doug Schallmoser <dougschallmoser@mgail.com>
2020-10-14 22:38:12 -0700

    refactor code with new bindings

--- gastropoda-frontend/src/components/entries.js
+++ gastropoda-frontend/src/components/entries.js
@@ -2,79 +2,69 @@ class Entries {
   constructor() {
     this.entries = []
     this.adapter = new EntriesAdapter()
-    this.loadEntries()
+    // this.loadEntries()
     this.setBindings()
     this.setEventListeners()
   }
 
   setBindings() {
-    this.container = document.querySelector('.container')
-    this.newForm = document.querySelector('.new-form')
-    this.entriesContainer = document.querySelector('.entries-container')
+    this.navBar = document.getElementById('nav-bar')
+    this.mainContent = document.getElementById('main-content')
     this.newEntryImage = document.getElementById('image')
-    this.loadFormButton = document.getElementById('button-load-form')
   }
 
   setEventListeners() {
-    this.loadFormButton.addEventListener('click', this.loadForm.bind(this))
+    this.navBar.getElementsByTagName('a')[2].addEventListener('click', this.loadForm(this))
   }
 
   loadForm() {
-    if (document.querySelector('.entry-form')) {
-      this.closeForm()
-    } else {
-      const formDiv = document.createElement('div')
-      formDiv.className = "entry-form"
-      formDiv.innerHTML = `
-      <form id="entry-form">
-        <label for="title">Title:</label>
-        <input type="text" id="title"><p></p>
-        <label for="author_name">Author Name:</label>
-        <input type="text" id="author_name"><p></p>
-        <label for="author_bio">Author Bio:</label>
-        <input type="text" id="author_bio" name="author_bio"><p></p>
-        <label for="content">Content:</label>
-        <input type="text" id="content"><p></p>
-        <label for="image">Image URL:</label>
-        <input type="text" id="image"><p></p>
-        <input type="submit">
-      </form>`
-      this.newForm.append(formDiv)
-      this.loadFormButton.innerText = "- CLOSE"
+    if (!document.querySelector('.entry-form')) {
+      const form = document.createElement('div')
+      form.className = "entry-form-div"
+      form.innerHTML = `
+        <form id="entry-form">
+          <label for="title">Title:</label>
+          <input type="text" id="title"><p></p>
+          <label for="author_name">Author Name:</label>
+          <input type="text" id="author_name"><p></p>
+          <label for="author_bio">Author Bio:</label>
+          <input type="text" id="author_bio" name="author_bio"><p></p>
+          <label for="content">Content:</label>
+          <input type="text" id="content"><p></p>
+          <label for="image">Image URL:</label>
+          <input type="text" id="image"><p></p>
+          <input type="submit">
+        </form>`
+      this.mainContent.append(form)
       this.entryForm = document.getElementById('entry-form')
       this.entryForm.addEventListener('submit', this.createEntry.bind(this))
-    }
-  }
-
-  closeForm() {
-    document.querySelector('.entry-form').remove()
-    this.loadFormButton.innerText = "+ SUBMIT YOUR STORY"
+    } 
   }
 
   createEntry(event) {
     event.preventDefault()
-    const imageValue = this.newEntryImage.value
-    this.adapter.createEntry(imageValue).then(entry => {
+    const formImageValue = document.getElementById('image').value
+    this.adapter.createEntry(formImageValue).then(entry => {
       this.entries.push(new Entry(entry))
-      this.newEntryImage.value = ''
+      document.getElementById('image').value = ''
       this.render()
     })
   }
 
-  loadEntries() {
-    this.adapter.getEntries().then(entries => {
-      entries.forEach(entry => this.entries.push(new Entry(entry)))
-    })
-    .then(() => {
-      this.render()
-    })
-  }
+  // loadEntries() {
+  //   this.adapter.getEntries().then(entries => {
+  //     entries.forEach(entry => this.entries.push(new Entry(entry)))
+  //   })
+  //   .then(() => {
+  //     this.render()
+  //   })
+  // }
 
-  render() {
-    this.entriesContainer.innerHTML = ''
-    this.entries.map(entry => {
-      this.entriesContainer.appendChild(entry.renderItem())
-    })
-  }
+  // render() {
+  //   this.entriesContainer.innerHTML = ''
+  //   this.entries.map(entry => {
+  //     this.entriesContainer.appendChild(entry.renderItem())
+  //   })
+  // }
 
 }
