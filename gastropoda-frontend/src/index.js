2bd74975a7e252978a37d002c30fa23758a6f37c
Doug Schallmoser <dougschallmoser@mgail.com>
2020-10-15 21:30:54 -0700

    update html

--- gastropoda-frontend/src/index.js
+++ gastropoda-frontend/src/index.js
@@ -9,7 +9,11 @@ function renderAbout() {
   if (!document.getElementById('about')) {
     div = `
       <div id="about">
-        I'm baby tofu paleo scenester, twee drinking vinegar irony gastropub craft beer post-ironic organic. Locavore adaptogen actually, flannel selvage humblebrag taiyaki offal pug organic sriracha +1. Deep v VHS lumbersexual farm-to-table snackwave, squid wolf. Ugh cold-pressed subway tile XOXO, 90's hella vaporware celiac ethical polaroid fingerstache forage whatever seitan adaptogen.
+        <p><em>Gastropoda</em> is a literary magazine that seeks to celebrate the spiralic nature of life.</p>
+
+        <p><em>Gastropoda</em> started out as an idea, grew into a fascination, and now blossoms into physical manifestation.</p>
+        
+        <p><em>Gastropoda</em> aims to slow you down and spin you around, to help you see the beauty and tragedy in all of life’s twists and turns, and to show you that there is no such thing as wasted time.</p>
       </div>
     `
     mainContent.innerHTML = div
