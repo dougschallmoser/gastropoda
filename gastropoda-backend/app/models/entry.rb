bdec58242bd9ad220e43d0a4051c9c1ee6801be7
Doug Schallmoser <dougschallmoser@mgail.com>
2020-10-16 14:17:23 -0700

    add presence validations

--- gastropoda-backend/app/models/entry.rb
+++ gastropoda-backend/app/models/entry.rb
@@ -1,5 +1,6 @@
 class Entry < ApplicationRecord
   has_many :comments
+  validates :title, :author_name, :author_bio, :content, :image, presence: true
 
   def created_at
     attributes['created_at'].strftime("%B %e, %Y")
