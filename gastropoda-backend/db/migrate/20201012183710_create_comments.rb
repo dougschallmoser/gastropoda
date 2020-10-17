8c4dccd346ee19deca6ae86fa478eb06085b472a
Doug Schallmoser <dougschallmoser@mgail.com>
2020-10-17 08:50:04 -0700

    add new comment attributes

--- gastropoda-backend/db/migrate/20201012183710_create_comments.rb
+++ gastropoda-backend/db/migrate/20201012183710_create_comments.rb
@@ -1,6 +1,8 @@
 class CreateComments < ActiveRecord::Migration[6.0]
   def change
     create_table :comments do |t|
+      t.string :name
+      t.string :email
       t.string :content
       t.integer :entry_id
 
