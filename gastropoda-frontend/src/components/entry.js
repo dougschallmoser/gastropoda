15855c52292b05acaa8ed726b1af579b8eb1db93
Doug Schallmoser <dougschallmoser@mgail.com>
2020-10-17 14:54:36 -0700

    add functionality for creating comments and render comments

--- gastropoda-frontend/src/components/entry.js
+++ gastropoda-frontend/src/components/entry.js
@@ -50,7 +50,7 @@ class Entry {
       div.id = "display-entry"
       div.innerHTML = `
         <img src="${entry.image}">
-        <p><h2>${entry.title.toUpperCase()}</h2></p>
+        <h2>${entry.title.toUpperCase()}</h2>
         <h4>${entry.author_name} <span id="date">${entry.created_at}</span></h4>
         <p>${entry.content}</p>`
       container.append(div)
@@ -59,11 +59,13 @@ class Entry {
       // ABOUT THE CONTRIBUTOR 
       const contributorDiv = document.createElement('div')
       const contributorHeading = document.createElement('div')
+      const hr = document.createElement('hr')
       contributorDiv.id = "contributor"
       contributorHeading.id = "contributor-heading"
       contributorHeading.innerHTML = '<span id="maincolor">+</span> ABOUT THE CONTRIBUTOR'
       contributorDiv.append(contributorHeading)
       div.append(contributorDiv)
+      contributorDiv.append(hr)
       contributorHeading.addEventListener('click', displayAuthorBio)
       function displayAuthorBio() {
         contributorHeading.innerHTML = '<span id="maincolor">-</span> ABOUT THE CONTRIBUTOR'
@@ -71,10 +73,10 @@ class Entry {
           document.getElementById('author-bio').remove()
           contributorHeading.innerHTML = '<span id="maincolor">+</span> ABOUT THE CONTRIBUTOR'
         } else {
-        const bio = document.createElement('div')
-        bio.id = "author-bio"
-        bio.innerText = entry.author_bio
-        contributorDiv.append(bio)
+          const bio = document.createElement('div')
+          bio.id = "author-bio"
+          bio.innerText = entry.author_bio
+          contributorDiv.insertBefore(bio, contributorDiv.childNodes[1])
         }
       }
 
@@ -83,59 +85,77 @@ class Entry {
       const commentsHeading = document.createElement('div')
       commentsDiv.id = "comments"
       commentsHeading.id = "comments-heading"
-      commentsHeading.innerHTML = '<span id="maincolor">+</span> COMMENTS'
+      commentsHeading.innerHTML = '<span id="maincolor">+</span> LEAVE A COMMENT'
       commentsDiv.append(commentsHeading)
       div.append(commentsDiv)
-      commentsHeading.addEventListener('click', displayComments)
-      function displayComments() {
-        commentsHeading.innerHTML = '<span id="maincolor">-</span> COMMENTS'
-        if (document.getElementById('display-comments')) {
-          document.getElementById('display-comments').remove()
-          commentsHeading.innerHTML = '<span id="maincolor">+</span> COMMENTS'
+      commentsHeading.addEventListener('click', displayCommentForm)
+      function displayCommentForm() {
+        commentsHeading.innerHTML = '<span id="maincolor">-</span> LEAVE A COMMENT'
+        if (document.getElementById('make-comment')) {
+          document.getElementById('make-comment').remove()
+          commentsHeading.innerHTML = '<span id="maincolor">+</span> LEAVE A COMMENT'
         } else {
-        const comments = document.createElement('div')
-        comments.id = "display-comments"
-        commentsDiv.append(comments)
-        self.displayComments()
-        // comments.innerText = "Hello"
-        // commentsDiv.append(comments)
+          const makeComment = document.createElement('div')
+          makeComment.id = "make-comment"
+          commentsDiv.insertBefore(makeComment, commentsDiv.childNodes[1])
+          makeComment.innerHTML = 
+            `<form id="comment-form">
+              <div class="comment-form-name">
+                <input type="text" id="name" name="name" placeholder="Your Name">
+              </div>
+              <div class="comment-form-email">
+                <input type="text" id="email" name="email" placeholder="Your Email (required)">
+              </div>
+              <div class="comment-form-content">
+                <input type="text" id="comment-content" name="content" placeholder="Add a public comment...">
+              </div>
+              <div class="comment-form-submit">
+                <input type="submit" value="Comment">
+              </div>
+            </form>`
+          const form = document.getElementById('comment-form')
+          form.addEventListener('submit', createComment)
+          function createComment(event) {
+            event.preventDefault()
+            const nameValue = document.getElementById('name').value 
+            const emailValue = document.getElementById('email').value
+            const contentValue = document.getElementById('comment-content').value
+            const formValues = {
+              name: nameValue,
+              email: emailValue,
+              content: contentValue,
+              entry_id: self.id
+            }
+            new CommentsAdapter().createComment(formValues).then(comment => {
+              self.comments.push(comment)
+              renderComments()
+            })
+          }
+
         }
       }
 
+      function renderComments() {
+        self.comments.forEach(comment => {
+          const showCommentDiv = document.createElement('div')
+          showCommentDiv.id = "show-comment"
+          showCommentDiv.innerHTML = 
+            `<span id="comment-name">${comment.name}</span> <span id="comment-created">${comment.created_at}</span>
+             <div id="comment-content">${comment.content}</div`
+          if (document.getElementById('make-comment')) {
+            const makeComment = document.getElementById('make-comment')
+            makeComment.parentNode.insertBefore(showCommentDiv, makeComment.nextSibling);
+          } else {
+            const commentsHeading = document.getElementById('comments-heading')
+            commentsHeading.parentNode.insertBefore(showCommentDiv, commentsHeading.nextSibling);
+          }
+        })
+      }
+
+      renderComments()
+
     })
     }
-  }
-
-  displayComments() {
-    const comments = document.getElementById('display-comments')
-    const showComments = document.createElement('div')
-    showComments.id = "show-comments"
-
-    if (this.comments.length === 0) {
-      showComments.innerHTML = "There are currently no comments."
-    } else {
-      this.comments.forEach(comment => showComments.innerHTML += comment)
-    }
-
-    comments.append(showComments)
-
-    const div =
-      `<form id="comment-form">
-        <div class="comment-form-name">
-          <input type="text" id="name" name="name" placeholder="Your Name">
-        </div>
-        <div class="comment-form-email">
-          <input type="text" id="email" name="email" placeholder="Your Email (required)">
-        </div>
-        <div class="comment-form-content">
-          <input type="text" id="comment-content" name="content" placeholder="Add a public comment...">
-        </div>
-        <div class="comment-form-submit">
-          <input type="submit" value="Comment">
-        </div>
-      </form>`
-
-    comments.innerHTML = div
   }
   
 }
