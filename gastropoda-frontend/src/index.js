924c3bb30a63fb84337520e7c6773a65c2beafb9
Doug Schallmoser <dougschallmoser@mgail.com>
2020-10-19 09:47:53 -0700

    added word to about text

--- gastropoda-frontend/src/index.js
+++ gastropoda-frontend/src/index.js
@@ -9,7 +9,7 @@ function renderAbout() {
   if (!document.getElementById('about')) {
     div = `
       <div id="about">
-        <p><span id="gastropoda">GASTROPODA</span> is a literary magazine that seeks to celebrate the spiralic nature of life. What started out as an idea, grew into a fascination, and now blossoms into physical manifestation. This aims to slow you down and spin you around, to help you see the beauty and tragedy in all of life’s twists and turns, and to show you that there is no such thing as wasted time.</p>
+        <p><span id="gastropoda">GASTROPODA</span> is a literary magazine that seeks to celebrate the spiralic nature of life. What started out as an idea, grew into a fascination, and now blossoms into physical manifestation. This magazine aims to slow you down and spin you around, to help you see the beauty and tragedy in all of life’s twists and turns, and to show you that there is no such thing as wasted time.</p>
       </div>
     `
     mainContent.innerHTML = div
