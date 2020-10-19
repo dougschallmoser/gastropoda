e7eb09a728081fcd3b2dad3f1225c0e62dd99926
Doug Schallmoser <dougschallmoser@mgail.com>
2020-10-18 21:58:38 -0700

    add span around gastropoda word

--- gastropoda-frontend/src/index.js
+++ gastropoda-frontend/src/index.js
@@ -9,11 +9,7 @@ function renderAbout() {
   if (!document.getElementById('about')) {
     div = `
       <div id="about">
-        <p><em>Gastropoda</em> is a literary magazine that seeks to celebrate the spiralic nature of life.</p>
-
-        <p><em>Gastropoda</em> started out as an idea, grew into a fascination, and now blossoms into physical manifestation.</p>
-        
-        <p><em>Gastropoda</em> aims to slow you down and spin you around, to help you see the beauty and tragedy in all of life’s twists and turns, and to show you that there is no such thing as wasted time.</p>
+        <p><span id="gastropoda">GASTROPODA</span> is a literary magazine that seeks to celebrate the spiralic nature of life. What started out as an idea, grew into a fascination, and now blossoms into physical manifestation. This aims to slow you down and spin you around, to help you see the beauty and tragedy in all of life’s twists and turns, and to show you that there is no such thing as wasted time.</p>
       </div>
     `
     mainContent.innerHTML = div
