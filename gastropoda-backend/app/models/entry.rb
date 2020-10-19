1839b7cde6ffc68b5c6c8677b8a9f6fc5cbf38be
Doug Schallmoser <dougschallmoser@mgail.com>
2020-10-19 09:19:44 -0700

    add scope method to sort by id

--- gastropoda-backend/app/models/entry.rb
+++ gastropoda-backend/app/models/entry.rb
@@ -1,6 +1,7 @@
 class Entry < ApplicationRecord
   has_many :comments
   validates :title, :author_name, :author_bio, :content, :image, presence: true
+  scope :by_id, -> {order("id ASC")}
 
   def created_at
     attributes['created_at'].strftime("%B %e, %Y")
