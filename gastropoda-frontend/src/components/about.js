e134306cd452aa6ebb59d2639337ea73d55ff261
Doug Schallmoser <dougschallmoser@mgail.com>
2020-10-20 16:45:32 -0700

    clean up

--- gastropoda-frontend/src/components/about.js
+++ gastropoda-frontend/src/components/about.js
@@ -18,7 +18,7 @@ class About {
       this.mainContent.innerHTML =
       `<div id="about">
           <p><span id="gastropoda">GASTROPODA</span> is a literary magazine that seeks to celebrate the spiralic nature of life. What started out as an idea grew into a fascination, and now blossoms into physical manifestation. This magazine aims to slow you down and spin you around, to help you see the beauty and tragedy in all of life’s twists and turns, and to show you that there is no such thing as wasted time.</p>
-        </div>`
+      </div>`
     }
   }
 }
