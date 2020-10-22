fda3de7f4ca8afed49f29d425f8bc993479e0442
Doug Schallmoser <dougschallmoser@mgail.com>
2020-10-21 18:05:27 -0700

    add fiction attribute

--- gastropoda-backend/app/serializers/entry_serializer.rb
+++ gastropoda-backend/app/serializers/entry_serializer.rb
@@ -1,4 +1,4 @@
 class EntrySerializer < ActiveModel::Serializer 
-  attributes :id, :title, :author_name, :author_bio, :content, :likes, :image, :created_at
+  attributes :id, :title, :author_name, :author_bio, :fiction, :content, :likes, :image, :created_at
   has_many :comments
 end
