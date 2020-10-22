d6fc25ccad699c83d0f4ed04d9eb869b4f075174
Doug Schallmoser <dougschallmoser@mgail.com>
2020-10-21 18:05:12 -0700

    add validation for fiction attribute

--- gastropoda-backend/app/models/entry.rb
+++ gastropoda-backend/app/models/entry.rb
@@ -1,5 +1,6 @@
 class Entry < ApplicationRecord
   has_many :comments
+  validates_inclusion_of :fiction, in: [true, false], :message => "or Creative Non-Fiction can't be blank"
   validates :title, :author_name, :author_bio, :content, :image, presence: true
   scope :by_id, -> {order("id ASC")}
 
