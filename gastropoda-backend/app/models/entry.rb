b70d54ff3ff9ef3cc4ad82857876a44660ec24cc
Doug Schallmoser <dougschallmoser@mgail.com>
2020-10-23 08:47:44 -0700

    add dependent destroy to entry

--- gastropoda-backend/app/models/entry.rb
+++ gastropoda-backend/app/models/entry.rb
@@ -1,5 +1,5 @@
 class Entry < ApplicationRecord
-  has_many :comments
+  has_many :comments, dependent: :destroy
   validates_inclusion_of :fiction, in: [true, false], :message => "or Creative Nonfiction can't be blank"
   validates :title, :author_name, :author_bio, :content, :image, presence: true
   scope :by_id, -> {order("id ASC")}
