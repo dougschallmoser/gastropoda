1feaa8f0af02c3c1c5f4d92fac47f52b7e54256f
Doug Schallmoser <dougschallmoser@mgail.com>
2020-10-23 12:34:12 -0700

    add comment.js file, move comment methods from entry.js to comment.js

--- gastropoda-frontend/src/components/entry.js
+++ gastropoda-frontend/src/components/entry.js
@@ -2,7 +2,6 @@ class Entry {
 
   static allEntries = []
   static mainContent = document.getElementById('main-content')
-  static navBar = document.getElementById('nav-bar')
 
   constructor(entry) {
     this.id = entry.id
@@ -14,7 +13,7 @@ class Entry {
     this.likes = entry.likes
     this.image = entry.image
     this.created_at = entry.created_at
-    this.comments = entry.comments
+    this.comments = entry.comments.map(comment => new Comment(comment))
     Entry.allEntries.push(this)
   }
 
@@ -299,116 +298,8 @@ class Entry {
           contributorDiv.insertBefore(bio, contributorDiv.childNodes[1])
         }
       })
-      this.loadCommentForm()
-      this.comments.forEach(comment => this.renderComment(comment))
-    }
-  }
-
-  renderComment(comment) {
-    const showCommentDiv = document.createElement('div')
-    showCommentDiv.className = "show-comment"
-    showCommentDiv.innerHTML = 
-      `<span id="comment-name">${comment.name}</span> <span id="comment-created">${comment.created_at}</span>
-       <div id="comment-content">${comment.content}</div`
-    if (document.getElementById('make-comment')) {
-      const makeComment = document.getElementById('make-comment')
-      makeComment.parentNode.insertBefore(showCommentDiv, makeComment.nextSibling);
-    } else {
-      const commentsHeading = document.getElementById('comments-heading')
-      commentsHeading.parentNode.insertBefore(showCommentDiv, commentsHeading.nextSibling);
-    }
-  }
-
-  loadCommentForm() {
-    const div = document.getElementById('display-entry')
-    const commentsDiv = document.createElement('div')
-    const commentsHeading = document.createElement('div')
-    commentsDiv.id = "comments"
-    commentsHeading.id = "comments-heading"
-    commentsHeading.innerHTML = '<span id="maincolor">+</span> LEAVE A COMMENT'
-    commentsDiv.append(commentsHeading)
-    div.append(commentsDiv)
-    commentsHeading.addEventListener('click', displayCommentForm.bind(this))
-    
-    function displayCommentForm() {
-      commentsHeading.innerHTML = '<span id="maincolor">-</span> LEAVE A COMMENT'
-      if (document.getElementById('make-comment')) {
-        document.getElementById('make-comment').remove()
-        commentsHeading.innerHTML = '<span id="maincolor">+</span> LEAVE A COMMENT'
-      } else {
-        const makeComment = document.createElement('div')
-        makeComment.id = "make-comment"
-        commentsDiv.insertBefore(makeComment, commentsDiv.childNodes[1])
-        makeComment.innerHTML = 
-          `<form id="comment-form">
-            <div class="comment-form-name">
-              <input type="text" id="name" name="name" placeholder="Your Name">
-            </div>
-            <div class="comment-form-email">
-              <input type="text" id="email" name="email" placeholder="Your Email (required)">
-            </div>
-            <div class="comment-form-content">
-              <input type="text" id="comment-content" name="content" placeholder="Add a public comment...">
-            </div>
-            <div class="comment-form-submit">
-              <input type="submit" value="Comment">
-            </div>
-          </form>`
-        const form = document.getElementById('comment-form')
-        form.addEventListener('submit', (event) => {
-          event.preventDefault()
-          const formValues = {
-            name: document.getElementById('name').value,
-            email: document.getElementById('email').value,
-            content: document.getElementById('comment-content').value,
-            entry_id: this.id
-          }
-          new CommentsAdapter().createComment(formValues).then(comment => {
-            this.renderModal(comment)
-          })
-          .catch(error => {this.renderModal(error)})
-        })
-      }
-    }
-  }
-
-  renderModal(comment, subject = "comment") {
-    const div = document.createElement('div')
-    const contentDiv = document.createElement('div')
-    const span = document.createElement('span')
-    const p = document.createElement('p')
-    const p2 = document.createElement('p')
-    div.id = "myModal"
-    div.className = "modal"
-    contentDiv.className = "modal-content"
-    span.className = "close"
-    span.innerHTML = `&times;`
-    contentDiv.append(span)
-    if (comment.message) {
-      if (Array.isArray(comment.message)) {
-        comment.message.forEach(message => {
-          p2.innerHTML += `<li>${message}</li>`
-        })
-      } else {
-        p2.innerHTML = comment.message
-      }
-      p.innerHTML = `Your ${subject} did <strong>not</strong> save because...`
-      contentDiv.append(p)
-      contentDiv.append(p2)
-      div.append(contentDiv)
-      div.style.display = "block"
-      document.querySelector('#main-content').append(div)
-      span.addEventListener('click', () => {div.remove()})
-      window.addEventListener('click', function(event) {
-        if (event.target == div) {div.remove()}
-      })
-    } else {
-      this.comments.push(comment)
-      this.renderComment(comment)
-      document.getElementById('comment-count').innerHTML++
-      document.getElementById('name').value = ''
-      document.getElementById('email').value = ''
-      document.getElementById('comment-content').value = ''
+      Comment.displayCommentForm(this.id)
+      this.comments.forEach(comment => comment.renderComment())
     }
   }
 
