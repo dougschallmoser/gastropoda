e5ebec7996a0ede2f85922de79a58fa157ab77df
Doug Schallmoser <dougschallmoser@gmail.com>
2020-12-05 09:11:54 -0800

    set routes for only create and destroy comments

--- gastropoda-backend/config/routes.rb
+++ gastropoda-backend/config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do 
       resources :entries
-      resources :comments
+      resources :comments, only: [:create, :destroy]
     end
   end
 end
