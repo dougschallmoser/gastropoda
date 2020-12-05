11767c2777fd24e4829f114671f0a96a5957fd4c
Doug Schallmoser <dougschallmoser@gmail.com>
2020-12-05 10:47:54 -0800

    add update route for comments

--- gastropoda-backend/config/routes.rb
+++ gastropoda-backend/config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do 
       resources :entries
-      resources :comments, only: [:create, :destroy]
+      resources :comments, only: [:create, :destroy, :update]
     end
   end
 end
