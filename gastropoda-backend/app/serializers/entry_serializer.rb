71764e6d9fee47d0af3be29b17babc5ac8813b37
Doug Schallmoser <dougschallmoser@gmail.com>
2020-12-05 10:47:38 -0800

    add block to has many relationship to sort by id

--- gastropoda-backend/app/serializers/entry_serializer.rb
+++ gastropoda-backend/app/serializers/entry_serializer.rb
@@ -1,4 +1,6 @@
 class EntrySerializer < ActiveModel::Serializer 
   attributes :id, :title, :author_name, :author_bio, :fiction, :content, :likes, :image, :created_at
-  has_many :comments
+  has_many :comments do 
+    object.comments.order(:id)
+  end
 end
